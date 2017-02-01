e90eaa3953e8caab7b372a77701153212f6456d2
antowski <antowski@gmail.com>
2017-02-01 12:35:19 +0300

    remove unused classes

--- sonar-onec-plugin/src/main/java/org/antowski/sonar/plugins/onec/OneCPlugin.java
+++ sonar-onec-plugin/src/main/java/org/antowski/sonar/plugins/onec/OneCPlugin.java
@@ -20,27 +20,37 @@
 
 package org.antowski.sonar.plugins.onec;
 
-import static java.util.Arrays.asList;
-
 import org.sonar.api.Plugin;
 import org.sonar.api.config.PropertyDefinition;
-
-//import com.google.common.collect.ImmutableList;
+import org.sonar.api.resources.Qualifiers;
 
 /**
  * This class is the entry point for all extensions. It is referenced in pom.xml.
  */
 public class OneCPlugin implements Plugin {
 
+  private static final String GENERAL = "General";
+
   public static final String FILE_SUFFIXES_KEY = "antowski.onec.file.suffixes";
 
   @Override
   public void define(Context context) {
     
-    context.addExtensions(OneCLanguage.class, OneCQualityProfile.class);
-    context.addExtension(OneCLanguageProperties.getProperties());
+    context.addExtensions(
 
-    context.addExtension(OneC.class);
+      PropertyDefinition.builder(FILE_SUFFIXES_KEY)
+        .name("File Suffixes")
+        .description("Comma-separated list of suffixes of 1C 7.7 Enterprise files to analyze.")
+        .subCategory(GENERAL)
+        .onQualifiers(Qualifiers.PROJECT)
+        .defaultValue("1s")
+        .build(),
+
+      OneC.class,
+
+      OneCQualityProfile.class
+      
+      );
    
   }
 
