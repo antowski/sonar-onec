6e0b2dcd94108632b80340535e9966b9864eb686
antowski <antowski@gmail.com>
2017-05-17 23:01:01 +0300

    IdentifierTree and ExpressionTree interfaces added

---
+++ onec-frontend/src/main/java/org/antowski/plugins/onec/api/tree/expression/ExpressionTree.java
@@ -0,0 +1,10 @@
+package org.antowski.plugins.onec.api.tree.expression;
+
+import org.antowski.plugins.onec.api.tree.Tree;
+
+/**
+ * Parent interface for all the trees representing expressions
+ */
+public interface ExpressionTree extends Tree {
+
+}
---
+++ onec-frontend/src/main/java/org/antowski/plugins/onec/api/tree/expression/IdentifierTree.java
@@ -0,0 +1,14 @@
+package org.antowski.plugins.onec.api.tree.expression;
+
+import org.antowski.plugins.onec.api.tree.lexical.SyntaxToken;
+
+/**
+ * Common interface to represent all kinds of identifiers.
+ */
+public interface IdentifierTree extends ExpressionTree {
+
+    SyntaxToken token();
+
+    String text();
+
+}
